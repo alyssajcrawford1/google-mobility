52ee114da1666e3e6f2bca0f565152333bfb4770
Alyssa Crawford <alyssacrawford@Alyssas-MacBook-Pro.local>
2022-06-23 11:16:26 -0700

    Added second plot, read in effects csv

--- covid_mobility_data/app.R
+++ covid_mobility_data/app.R
@@ -10,73 +10,9 @@
 library(tidyverse)
 library(effects)
 
-# read in data from all three years
-mobility_data_2020 <- read_csv("data/2020_US_Region_Mobility_Report.csv")
-mobility_data_2021 <- read_csv("data/2021_US_Region_Mobility_Report.csv")
-mobility_data_2022 <- read_csv("data/2022_US_Region_Mobility_Report.csv")
-
-tidy_data_2020 <- mobility_data_2020 %>%
-  pivot_longer(cols = retail_and_recreation_percent_change_from_baseline:residential_percent_change_from_baseline,
-               names_to = "place",
-               values_to = "percent_change_from_baseline") %>%
-  mutate(place = gsub("_percent_change_from_baseline", "", place))
-tidy_data_2021 <- mobility_data_2021 %>%
-  pivot_longer(cols = retail_and_recreation_percent_change_from_baseline:residential_percent_change_from_baseline,
-               names_to = "place",
-               values_to = "percent_change_from_baseline") %>%
-  mutate(place = gsub("_percent_change_from_baseline", "", place))
-tidy_data_2022 <- mobility_data_2022 %>%
-  pivot_longer(cols = retail_and_recreation_percent_change_from_baseline:residential_percent_change_from_baseline,
-               names_to = "place",
-               values_to = "percent_change_from_baseline") %>%
-  mutate(place = gsub("_percent_change_from_baseline", "", place))
-
-
-az_data_2020 <- tidy_data_2020 %>%
-  filter(sub_region_1 == "Arizona")
-az_data_2021 <- tidy_data_2021 %>%
-  filter(sub_region_1 == "Arizona")
-az_data_2022 <- tidy_data_2022 %>%
-  filter(sub_region_1 == "Arizona")
-
-
-model_2020 <- lm(percent_change_from_baseline ~ sub_region_1 + place, data = tidy_data_2020)
-model_2021 <- lm(percent_change_from_baseline ~ sub_region_1 + place, data = tidy_data_2021)
-model_2022 <- lm(percent_change_from_baseline ~ sub_region_1 + place, data = tidy_data_2022)
-az_model_2020 <- lm(percent_change_from_baseline ~ place, data = az_data_2020)
-az_model_2021 <- lm(percent_change_from_baseline ~ place, data = az_data_2021)
-az_model_2022 <- lm(percent_change_from_baseline ~ place, data = az_data_2022)
-
-
-us_mobility_2020 <- effect("place", model_2020) %>%
-  data.frame()
-us_mobility_2021 <- effect("place", model_2021) %>%
-  data.frame()
-us_mobility_2022 <- effect("place", model_2022) %>%
-  data.frame()
-az_mobility_2020 <- effect("place", az_model_2020) %>%
-  data.frame()
-az_mobility_2021 <- effect("place", az_model_2021) %>%
-  data.frame()
-az_mobility_2022 <- effect("place", az_model_2022) %>%
-  data.frame()
-
-
-us_mobility_2020$year <- 2020
-us_mobility_2021$year <- 2021
-us_mobility_2022$year <- 2022
-az_mobility_2020$year <- 2020
-az_mobility_2021$year <- 2021
-az_mobility_2022$year <- 2022
-
-
-#us_mobility <- merge(us_mobility_2020, us_mobility_2021, all=TRUE)
-#us_mobility <- merge(us_mobility, us_mobility_2022, all=TRUE)
-#az_mobility <- merge(az_mobility_2020, az_mobility_2021, all=TRUE)
-#az_mobility <- merge(az_mobility, az_mobility_2022, all=TRUE)
-
-us_mobility <- bind_rows(us_mobility_2020, us_mobility_2021, us_mobility_2022)
-az_mobility <- bind_rows(az_mobility_2020, az_mobility_2021, az_mobility_2022)
+# Read in effects csv
+us_mobility <- read_csv("data/us_place_effect_mobility.csv")
+az_mobility <- read_csv("data/az_place_effect_mobility.csv")
 
 year_options <- c(2020, 2021, 2022)
 
@@ -123,7 +59,7 @@ server <- function(input, output) {
           filter(year == input$year_chosen)
 
         data_to_plot %>%
-          ggplot(aes(y = reorder(place, fit),
+          ggplot(aes(y = place,
                      x = fit,
                      xmax = upper,
                      xmin = lower,
@@ -135,11 +71,38 @@ server <- function(input, output) {
                         data = chosen_year) +
           geom_vline(xintercept = 0) +
           geom_label(aes(label = format(fit, digits = 3)),
-                     data = year_chosen) +
+                     data = chosen_year, nudge_y=0.4) +
           labs(title = "Mobility Change in Arizona",
                subtitle = "across Arizona, pre-pandemic baseline",
                x = "mobility change percent change from baseline",
                y = "place")
+    })
+
+    output$error_plot_2 <- renderPlot({
+      data_to_plot <- us_mobility %>%
+        group_by(year)
+
+      chosen_year <- us_mobility %>%
+        filter(year == input$year_chosen)
+
+      data_to_plot %>%
+        ggplot(aes(y = place,
+                   x = fit,
+                   xmax = upper,
+                   xmin = lower,
+                   group = year)) +
+        geom_errorbar(width = 0.5,
+                      color = "grey") +
+        geom_errorbar(width = 0.5,
+                      color = "red",
+                      data = chosen_year) +
+        geom_vline(xintercept = 0) +
+        geom_label(aes(label = format(fit, digits = 3)),
+                   data = chosen_year, nudge_y=0.4) +
+        labs(title = "Mobility Change in the United States",
+             subtitle = "across US, pre-pandemic baseline",
+             x = "mobility change percent change from baseline",
+             y = "place")
     })
 }
 
